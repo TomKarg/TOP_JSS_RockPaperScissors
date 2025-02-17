e0c5f1cea2d1051b1a7adc53b8e2c10e48e7a534
TomKarg <22795138+TomKarg@users.noreply.github.com>
2025-02-17 22:47:21 +0100

    Make game playable in a 5 round game

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("Hello World");
 
 const choice = ["rock", "paper", "scissors"];
-let humanScore = 0;
-let computerScore = 0;
+// let humanScore = 0;
+// let computerScore = 0;
 
 function getComputerChoice() {
   let rand = Math.floor(Math.random() * 3); //this gives us random 0, 1, 2
@@ -20,18 +20,46 @@ function getHumanChoice() {
   }
 }
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log(`DRAW! Both players choose ${computerChoice}!`);
-  } else if (
-    (humanChoice === "rock" && computerChoice == "scissors") ||
-    (humanChoice === "scissors" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "rock")
-  ) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
-    humanScore++;
-  } else {
-    console.log(`You lose! ${humanChoice} beats ${computerChoice}!`);
-    computerScore++;
+// function playRound(humanChoice, computerChoice) {
+//   if (humanChoice === computerChoice) {
+//     console.log(`DRAW! Both players choose ${computerChoice}!`);
+//   } else if (
+//     (humanChoice === "rock" && computerChoice == "scissors") ||
+//     (humanChoice === "scissors" && computerChoice === "paper") ||
+//     (humanChoice === "paper" && computerChoice === "rock")
+//   ) {
+//     console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+//     humanScore++;
+//   } else {
+//     console.log(`You lose! ${humanChoice} beats ${computerChoice}!`);
+//     computerScore++;
+//   }
+// }
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log(`DRAW! Both players choose ${computerChoice}!`);
+    } else if (
+      (humanChoice === "rock" && computerChoice == "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+      humanScore++;
+    } else {
+      console.log(`You lose! ${humanChoice} beats ${computerChoice}!`);
+      computerScore++;
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    console.log(`HUMAN: ${humanScore} - COMPUTER: ${computerScore}`);
   }
 }
