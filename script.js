033b9023b2e79040abd1c29bd58f70aa310eddf0
TomKarg <22795138+TomKarg@users.noreply.github.com>
2025-02-17 22:12:32 +0100

    Implement the function for computer and player choice

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+const choice = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let rand = Math.floor(Math.random() * 3); //this gives us random 0, 1, 2
+  return choice[rand];
+}
+
+//no  input validation implemented
+function getHumanChoice() {
+  while (true) {
+    let answer = prompt('Choose between "rock", "paper" or "scissors"');
+    answer = answer.toLowerCase();
+    if (choice.includes(answer)) {
+      return answer;
+    }
+  }
+}
