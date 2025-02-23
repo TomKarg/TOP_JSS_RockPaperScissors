9acd2f09e070c6979f79e414a61cbbcb7fc78bb2
TomKarg <22795138+TomKarg@users.noreply.github.com>
2025-02-23 15:38:04 +0100

    create js UI basic logic

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const GAMESTATE = ["ready", "playing", "finished"];
+let state = false;
+let humanScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll(".gameButton");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playGameUI(button.id);
+  });
+});
 console.log("Hello World");
 
 const choice = ["rock", "paper", "scissors"];
@@ -20,46 +31,50 @@ function getHumanChoice() {
   }
 }
 
-// function playRound(humanChoice, computerChoice) {
-//   if (humanChoice === computerChoice) {
-//     console.log(`DRAW! Both players choose ${computerChoice}!`);
-//   } else if (
-//     (humanChoice === "rock" && computerChoice == "scissors") ||
-//     (humanChoice === "scissors" && computerChoice === "paper") ||
-//     (humanChoice === "paper" && computerChoice === "rock")
-//   ) {
-//     console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
-//     humanScore++;
-//   } else {
-//     console.log(`You lose! ${humanChoice} beats ${computerChoice}!`);
-//     computerScore++;
-//   }
-// }
+//function to check the game state?
+function checkGameState() {
+  if (humanScore === 5 || computerScore === 5) {
+    humanScore = 0;
+    computerScore = 0;
+  }
+}
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+function checkWinner() {
+  const resDiv = document.querySelector("#finalResult");
+  if (humanScore === 5) {
+    resDiv.textContent = `You WIN with a score of ${humanScore} : ${computerScore}`;
+    console.log(`You WIN with a score of ${humanScore} : ${computerScore}`);
+  } else if (computerScore === 5) {
+    resDiv.textContent = `You lose with a SCORE OF ${humanScore} : ${computerScore}`;
+    console.log(`You lose with a SCORE OF ${humanScore} : ${computerScore}`);
+  }
+}
 
+function playGameUI(userInput) {
   function playRound(humanChoice, computerChoice) {
+    const outDiv = document.querySelector("#outputTxt");
     if (humanChoice === computerChoice) {
+      outDiv.textContent = `DRAW! Both players choose ${computerChoice}!`;
       console.log(`DRAW! Both players choose ${computerChoice}!`);
     } else if (
       (humanChoice === "rock" && computerChoice == "scissors") ||
       (humanChoice === "scissors" && computerChoice === "paper") ||
       (humanChoice === "paper" && computerChoice === "rock")
     ) {
+      outDiv.textContent = `You win! ${humanChoice} beats ${computerChoice}!`;
       console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
       humanScore++;
     } else {
+      outDiv.textContent = `You lose! ${humanChoice} beats ${computerChoice}!`;
       console.log(`You lose! ${humanChoice} beats ${computerChoice}!`);
       computerScore++;
     }
-  }
-
-  for (let i = 0; i < 5; i++) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
+    outDiv.textContent += ` HUMAN: ${humanScore} - COMPUTER: ${computerScore}`;
     console.log(`HUMAN: ${humanScore} - COMPUTER: ${computerScore}`);
   }
+
+  computerChoice = getComputerChoice();
+  checkGameState();
+  playRound(userInput, computerChoice);
+  checkWinner();
 }
